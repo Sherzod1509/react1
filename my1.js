c0a8695cab7b31f9094bb568c6468a461757622c
Sherzod1509 <ali.sher.tuit1509@mail.com>
2020-06-10 18:03:12 +0500

    2 коммит Sherzoda

--- my1.js
+++ my1.js
@@ -1,11 +1,35 @@
-let arr = ["albano", "chelentano", "chaykovsky", "gyete"];
+' use strict';
+// 2
+var money,
+    time;
+money = +prompt("Ваш бюджет на месяц?", "2500000");
 
-console.log(arr[2]);
+time = prompt("Введите дату в формате YYYY-MM-DD", "2020-02-02 ");
+// 2 вид решение то есть онлайн время
+// time = new Date();
+// console.log(time);
+//----------------4---------
+let ans1 = prompt("Введите обязательную статью расходов в этом месяце");
+//  let ans2 = prompt("Введите обязательную статью расходов в этом месяце");
+let ans3 = prompt("Во сколько обойдется?");
+//  let ans4 = prompt("Во сколько обойдется?");
 
-alert(arr[1]);
+//---------------3-------
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        otv1: ans1,
+        otv2: ans3
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+console.log("ответ на 1вопрос:", appData.expenses.otv1, "ответ на 2вопрос:", appData.expenses.otv2);
+console.log(appData.budget);
+console.log(appData.timeData);
+console.log(appData.savings);
 
-let answer = confirm("are you here my friend?");
-console.log(answer);
-
-let answer1 = prompt("Нравиться ли вам Футбол", "ДА");
-console.log(answer1);
+//  ------------------------5------------------------
+alert("by oy kunlik topgan pulingiz" +((appData.budget-appData.expenses.otv2)/30));
