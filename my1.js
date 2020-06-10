56a4b8d4af4489ba7a5ddfe1a36120e44157a6a8
Sherzod1509 <ali.sher.tuit1509@mail.com>
2020-06-10 17:56:03 +0500

    1 коммит

---
+++ my1.js
@@ -0,0 +1,11 @@
+let arr = ["albano", "chelentano", "chaykovsky", "gyete"];
+
+console.log(arr[2]);
+
+alert(arr[1]);
+
+let answer = confirm("are you here my friend?");
+console.log(answer);
+
+let answer1 = prompt("Нравиться ли вам Футбол", "ДА");
+console.log(answer1);
