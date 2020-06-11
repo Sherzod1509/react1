38526c6a8a7a6345444ad27761d3700c86b63d56
Sherzod1509 <ali.sher.tuit1509@mail.com>
2020-06-11 19:11:30 +0500

    mani 3kommitim mirzo

---
+++ my2.js
@@ -0,0 +1,31 @@
+'use_strict';
+// Какое будет выведено значение: let x = 5; alert( x++ );      =5
+
+// Что выведет этот код: let y = 1; let x = y = 2; alert(x);        =2
+
+// Что выведет этот код: alert( "1"[0] );       =1
+
+// Что выведет этот код: alert( null || 2 && 3 || 4 );      =3?????????????????????
+
+// Что выведет этот код: alert( +"Infinity" );      Infinity
+
+// Чему равно 2 && 1 && null && 0 && undefined ?    null ??
+
+// Есть ли разница между выражениями? !!( a && b ) и (a && b)?      bir xil ????
+
+// Правда ли что a == b ?       нет! ?????
+// let a = [1, 2, 3], b = [1, 2, 3]; 
+// if(a==b){console.log("massivlar bir xil");}
+// else{console.log("massivlar bir xil emas");}
+
+// Верно ли сравнение: 
+// console.log("ёжик" > "яблоко");     true???
+
+// Чему равно 
+// console.log(0 || "" || 2 || undefined || true || false); ==2 ????
+
+// Чему равно такое выражение: [ ] + false - null + true ?
+// console.log( [] + false - null + true); NaN 
+
+// Чему равна сумма [ ] + 1 + 2?
+// console.log(typeof( [] + 1 + 2)); =12 string bolgani ucun
